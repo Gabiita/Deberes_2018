3c30fd50abd91e89b4897c834da5e90d0de910ce
Gabiita <38547554+Gabiita@users.noreply.github.com>
2018-10-10 23:27:37 -0500

    Add files via upload

---
+++ Progra_Garcia.py
@@ -0,0 +1,36 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ()
+KeyboardInterrupt
+>>> print ("Escuela Politecnica Nacional")
+Escuela Politecnica Nacional
+>>> print ("Gabriela Garcia")
+Gabriela Garcia
+>>> trabajo = "Operaciones basicas"
+>>> print trabajo
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(trabajo)?
+>>> print (trabajo)
+Operaciones basicas
+>>> x=1+8
+>>> x
+9
+>>> 8+6
+14
+>>> print (x)
+9
+>>> print x
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(x)?
+>>> 8*7
+56
+>>> 9/2
+4.5
+>>> p1= "Programacion"
+>>> p2"avanzada"
+SyntaxError: invalid syntax
+>>> p2= "avanzada"
+
+>>> print (p1+" "p2)
+SyntaxError: invalid syntax
+>>> print (p1+" "+p2)
+Programacion avanzada
+>>> 
